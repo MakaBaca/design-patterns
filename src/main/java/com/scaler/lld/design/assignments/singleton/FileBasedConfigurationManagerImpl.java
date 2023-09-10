bf656ef6fe0d5085cb81ade41269ef9702a78b4c
Makeshkumar B <makesh1984@gmail.com>
2023-09-10 20:40:56 +0530

    Singleton solution

--- src/main/java/com/scaler/lld/design/assignments/singleton/FileBasedConfigurationManagerImpl.java
+++ src/main/java/com/scaler/lld/design/assignments/singleton/FileBasedConfigurationManagerImpl.java
@@ -1,50 +1,59 @@
 package com.scaler.lld.design.assignments.singleton;
 
 public class FileBasedConfigurationManagerImpl extends FileBasedConfigurationManager {
+    
+    private static FileBasedConfigurationManagerImpl configMgr = null;
 
+    private FileBasedConfigurationManagerImpl(){
+    }
     @Override
     public String getConfiguration(String key) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getConfiguration'");
+        return configMgr.properties.getProperty(key);
     }
 
     @Override
     public <T> T getConfiguration(String key, Class<T> type) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getConfiguration'");
+        String val = configMgr.properties.getProperty(key);
+        if(val==null) return null;
+        try{
+            return type.getConstructor(String.class).newInstance(val);
+        }catch (Exception e){
+            return null;
+        }
     }
 
     @Override
     public void setConfiguration(String key, String value) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setConfiguration'");
+        configMgr.properties.setProperty(key,value);
     }
 
     @Override
     public <T> void setConfiguration(String key, T value) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setConfiguration'");
+        configMgr.properties.setProperty(key, value.toString());
     }
 
     @Override
     public void removeConfiguration(String key) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeConfiguration'");
+        configMgr.properties.remove(key);
     }
 
     @Override
     public void clear() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+        configMgr.properties.clear();
     }
-
+    
     public static FileBasedConfigurationManager getInstance() {
-        // TODO Auto-generated method stub
-        return null;
+        if(configMgr == null){
+            synchronized (FileBasedConfigurationManagerImpl.class) {
+                if(configMgr == null)
+                    configMgr = new FileBasedConfigurationManagerImpl();
+            }
+        }
+        return configMgr;
     }
 
     public static void resetInstance() {
-        // TODO Auto-generated method stub
+        configMgr = null;
     }
 
 }
