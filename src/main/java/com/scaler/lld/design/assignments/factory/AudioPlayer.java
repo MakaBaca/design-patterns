80579759b51147c7540b73b35573593515a82be9
Makeshkumar B <makesh1984@gmail.com>
2023-10-08 17:51:30 +0530

    Solution for Simple Factory

--- src/main/java/com/scaler/lld/design/assignments/factory/AudioPlayer.java
+++ src/main/java/com/scaler/lld/design/assignments/factory/AudioPlayer.java
@@ -1,6 +1,14 @@
 package com.scaler.lld.design.assignments.factory;
 
 public abstract class AudioPlayer {
-
+    int volume;
+    double playBackRate;
     public abstract MediaFormat supportsType();
+
+    public abstract void play();
+
+    public abstract void pause();
+
+    public abstract void stop();
+
 }
--- src/main/java/com/scaler/lld/design/assignments/factory/AudioPlayerFactory.java
+++ src/main/java/com/scaler/lld/design/assignments/factory/AudioPlayerFactory.java
@@ -1,4 +1,14 @@
 package com.scaler.lld.design.assignments.factory;
 
 public class AudioPlayerFactory {
+
+    public static AudioPlayer createAudioPlayer(MediaFormat format, int volume, double playbackRate){
+
+        return switch (format) {
+            case FLAC -> new FLACPlayer(volume, playbackRate);
+            case WAV -> new WAVPlayer(volume, playbackRate);
+            default -> new MP3Player(volume, playbackRate);
+        };
+
+    }
 }
